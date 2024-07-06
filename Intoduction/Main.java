54f231878a1316a51e1edb5dda532dc9eda2f30d
RahulB <rahulbisht1012@gmail.com>
2024-07-06 13:23:35 +0530

    Introduction to Object Oriented Programming done

--- Intoduction/Main.java
+++ Intoduction/Main.java
@@ -1,4 +1,4 @@
-package Chapter1.Intro;
+package Intoduction;
 
 import java.util.Arrays;
 
--- Intoduction/WrapperExample.java
+++ Intoduction/WrapperExample.java
@@ -1,4 +1,4 @@
-package Chapter1.Intro;
+package Intoduction;
 
 public class WrapperExample {
     public static void main(String[] args) {
